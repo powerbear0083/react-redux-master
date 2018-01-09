970d63a6374bf0bcba07216dd55dd3ec22b051f8
yinhsun <powerbear0083@gmail.com>
2018-01-09 12:34:05 +0800

    chaper 07 practice done

--- chapter-07/weather_redux/src/weather/actions.js
+++ chapter-07/weather_redux/src/weather/actions.js
@@ -1,5 +1,7 @@
 import {FETCH_STARTED, FETCH_SUCCESS, FETCH_FAILURE} from './actionTypes.js';
 
+let nextSeqId = 0;
+
 export const fetchWeatherStarted = () => ({
   type: FETCH_STARTED
 });
@@ -17,26 +19,62 @@ export const fetchWeatherFailure = (error) => ({
 export const fetchWeather = (cityCode) => {
   return (dispatch) => {
     const apiUrl = `/data/cityinfo/${cityCode}.html`;
+    const seqId = ++ nextSeqId;
 
-    dispatch(fetchWeatherStarted());
+    console.log('seqId----', seqId)
 
-    return fetch(apiUrl).then((response) => {
-      if(response.status !== 200) {
-        throw new Error('Fail to get response with status' + response.status);
+    const dispatchIfValid = (action) => {
+      console.log('seqId----XXX', seqId);
+      console.log('seqId----YYY', nextSeqId)
+      if (seqId === nextSeqId) {
+        return dispatch(action);
+      }
+    }
+
+    dispatchIfValid(fetchWeatherStarted());
+
+    fetch(apiUrl).then( (response) => {
+      if (response.status !== 200) {
+        throw new Error('Fail to get response with status ' + response.status);
       }
 
-      response.json().then((responseJson) => {
-        dispatch(fetchWeatherSuccess(responseJson.weatherinfo));
-      }).catch((error) => {
-        dispatch(fetchWeatherFailure(error));
+      response.json().then( (responseJson) => {
+        dispatchIfValid(fetchWeatherSuccess(responseJson.weatherinfo));
+      }).catch( (error) => {
+        dispatchIfValid(fetchWeatherFailure(error));
       });
 
-    }).catch((error) => {
-      dispatch(fetchWeatherFailure(error));
+    }).catch( (error) => {
+      dispatchIfValid(fetchWeatherFailure(error));
     });
 
   }
 }
+
+// 不能中止 ajax 的寫法
+// export const fetchWeather = (cityCode) => {
+//   return (dispatch) => {
+//     const apiUrl = `/data/cityinfo/${cityCode}.html`;
+
+//     dispatch(fetchWeatherStarted());
+
+//     return fetch(apiUrl).then((response) => {
+//       if(response.status !== 200) {
+//         throw new Error('Fail to get response with status' + response.status);
+//       }
+
+//       response.json().then((responseJson) => {
+//         dispatch(fetchWeatherSuccess(responseJson.weatherinfo));
+//       }).catch((error) => {
+//         dispatch(fetchWeatherFailure(error));
+//       });
+
+//     }).catch((error) => {
+//       dispatch(fetchWeatherFailure(error));
+//     });
+
+//   }
+// }
 
 
 
